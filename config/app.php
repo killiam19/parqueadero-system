533758ab5bc7c5e9b70d4262bf3a113dd0c5d0e4
Killiam <killiam1119@gmail.com>
2025-07-28 11:03:42 -0500

    Credenciales de conexión a BD en producción

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
-
+/* 
+// Configuración local comentada
 return[
     'host' => '127.0.0.1',
     'dbname' => 'parqueadero_system',
@@ -8,5 +9,19 @@ return[
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]
+]; */
+
+// Configuración de Railway (sincronizada con config.php)
+return[
+    'host' => 'interchange.proxy.rlwy.net:18270', // Incluye el puerto
+    'dbname' => 'railway',
+    'username' => 'root',
+    'password' => 'XaUYILSXnQZKykDunwXNFIvrSThFFxDn',
+    'charset' => 'utf8mb4',
+    'options' => [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_TIMEOUT => 5,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
 ];
